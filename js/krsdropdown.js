fa78f50584ab4b4e3b05f41f7015c96e34381084
alexmcnurlin <alemcnurlin@gmail.com>
2014-03-21 20:40:53 -0600

    create test jquery for dropdown

---
+++ js/krsdropdown.js
@@ -0,0 +1,6 @@
+$(document).ready().on(click("#krs"), function(){
+  $(this).find("#krsdropdown").addclass(test)
+  })
+$(document).ready().on(click("#krsdropdown"), function(){
+  $(this).removeclass(test)
+  })
