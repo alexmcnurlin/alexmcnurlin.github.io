2d8fe5999f57bf08a2f3ba93f4a5614b35929ac0
alexmcnurlin <alemcnurlin@gmail.com>
2014-04-02 17:39:22 -0600

    delete broken javascript from previous commit

--- js/krsdropdown.js
+++
@@ -1,6 +0,0 @@
-$(document).ready().on(click("#krs"), function(){
-  $(this).find("#krsdropdown").addclass(test)
-  })
-$(document).ready().on(click("#krsdropdown"), function(){
-  $(this).removeclass(test)
-  })
